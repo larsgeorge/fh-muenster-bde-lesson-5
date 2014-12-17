c6e130fda6fb0ab776474a042ae663701c449db8
Lars George <lars@cloudera.com>
2014-12-17 10:33:39 +0100

    Added Hush from HBase book repo.

---
+++ src/main/java/org/fhmuenster/bde/hush/model/ShortDomain.java
@@ -0,0 +1,22 @@
+package org.fhmuenster.bde.hush.model;
+
+import java.util.List;
+
+public class ShortDomain {
+  private final String shortDomain;
+  private final List<String> domains;
+
+  public ShortDomain(String shortDomain, List<String> domains) {
+    super();
+    this.shortDomain = shortDomain;
+    this.domains = domains;
+  }
+
+  public String getShortDomain() {
+    return shortDomain;
+  }
+
+  public List<String> getDomains() {
+    return domains;
+  }
+}
---
+++ src/main/java/org/fhmuenster/bde/hush/model/TimeFrame.java
@@ -0,0 +1,8 @@
+package org.fhmuenster.bde.hush.model;
+
+/**
+ * Time frame for statistics.
+ */
+public enum TimeFrame {
+  DAY, WEEK, MONTH
+}
